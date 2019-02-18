63e59ec4a6f96ab95499048e378045c7e0518f4b
eebarmsoft <47724801+eebarmsoft@users.noreply.github.com>
2019-02-18 18:12:31 +0500

    Add files via upload

--- login.php
+++ login.php
@@ -4,13 +4,12 @@
 	<title>VERSUS</title>
 	<meta charset="UTF-8">
     <style>
-        #login{
-            position: relative;
-            top :-101px;
-            
+        .wrong_passowrd{
+            position:absolute;
+            top: 370px;
+            left: 815px;
             z-index: 2;
             color: whitesmoke;
-            
             
         }
         #footer{
@@ -54,30 +53,86 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/img-01.jpg');">
 			<div class="wrap-login100 p-t-190 p-b-30">
-				
-					<div class="login100-form-avatar" id="login">
+				<form class="login100-form validate-form" method="POST" action="">
+					<div class="login100-form-avatar">
 						<img src="images/avatar-01.jpg" alt="AVATAR">
 					</div>
 
-					<span class="login100-form-title p-t-20 p-b-45" id="login">
-						<br><br>
-				<a href="login.php">		<input type="submit" value="Login" name="submit" class="login100-form-btn"/></a>
-							
+					<span class="login100-form-title p-t-20 p-b-45">
 						
-                    
 					</span>
 
-					
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user"></i>
+						</span>
+					</div>
 
-					
+					<div class="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock"></i>
+						</span>
+					</div>
 
-					
-				
+					<div class="container-login100-form-btn p-t-10">
+						<input type="submit" value="Login" name="submit" class="login100-form-btn"/>
+							
+						
+                        
+					</div>
+
+					<div class="text-center w-full p-t-25 p-b-230">
+						<a href="#" class="txt1">
+							Forgot Username / Password?
+						</a>
+					</div>
+
+					<div class="text-center w-full">
+						<a class="txt1" href="#">
+							Create new account
+							<i class="fa fa-long-arrow-right"></i>						
+						</a>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
 	
 
+    
+    <?php
+ include ("connection.php");
+  session_start();
+
+
+if(isset($_POST['submit']))
+{
+   $username = $_POST['username'];
+   $password = $_POST['password'];
+    $query = "SELECT * FROM login WHERE username='$username' && password = '$password'";
+    $data = mysqli_query($con,$query);
+    $total = mysqli_num_rows($data);
+    if($total == 1)
+    {
+        $_SESSION['username']=$username;
+        $_SESSION['password'] =$password;
+        header('location:logincheck.php');
+        
+        
+      
+        
+        
+    
+    }else 
+         {
+          echo "<h6 class='wrong_passowrd'>Username or Password is Incorrect</h6>";
+         }
+}
+?>
     
    
     
